7afb673029f9e612cb337d76e3fb93e95c929239
legokidlogan <legokidlogan@gmail.com>
2021-02-02 14:37:47 -0700

    Fix typo

--- lua/autorun/server/sv_cfc_weapon_lockouts_init.lua
+++ lua/autorun/server/sv_cfc_weapon_lockouts_init.lua
@@ -1,4 +1,4 @@
-addCSLuaFile( "cfc_weapon_lockouts/client/cl_net.lua" )
-addCSLuaFile( "cfc_weapon_lockouts/shared/sh_base.lua" )
+AddCSLuaFile( "cfc_weapon_lockouts/client/cl_net.lua" )
+AddCSLuaFile( "cfc_weapon_lockouts/shared/sh_base.lua" )
 
 include( "cfc_weapon_lockouts/shared/sh_base.lua" )
