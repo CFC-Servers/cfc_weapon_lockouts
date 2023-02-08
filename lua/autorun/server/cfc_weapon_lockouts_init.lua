17fb425b5dc768f0f79d0621970e7c3cd7a5246d
Brandon Sturgeon <brandon@brandonsturgeon.com>
2023-02-08 14:50:20 -0800

    Watch ammo only

--- lua/autorun/server/cfc_weapon_lockouts_init.lua
+++ lua/autorun/server/cfc_weapon_lockouts_init.lua
@@ -1,40 +1,45 @@
-local IsValid = IsValid
-local CurTime = CurTime
-local ACT_VM_RELOAD = ACT_VM_RELOAD
+local function getSavedAmmo( ply )
+    local savedAmmo = ply.SavedAmmo
 
-local function setLockoutDuration( wep )
-    local reload = wep:SelectWeightedSequence( ACT_VM_RELOAD )
-    wep.CFC_LockoutTime = wep:SequenceDuration( reload )
+    if not savedAmmo then
+        ErrorNoHalt( "CFC_SavedAmmo: SavedAmmo table not found for " .. ply:Nick() )
+        savedAmmo = {}
+        ply.SavedAmmo = savedAmmo
+    end
+
+    return savedAmmo
 end
 
-hook.Add( "WeaponEquip", "CFC_WeaponLockouts", function( wep, ply )
+hook.Add( "WeaponEquip", "CFC_SavedAmmo_Restore", function( wep, ply )
     local wepClass = wep:GetClass()
+    local savedAmmo = getSavedAmmo( ply )
+    if not savedAmmo then return end
 
-    -- Sequence information and SetNextPrimaryFire are not available until the next frame
-    timer.Simple( 0, function()
-        if not IsValid( wep ) then return end
-        if not IsValid( ply ) then return end
+    savedAmmo = savedAmmo[wepClass]
+    if not savedAmmo then return end
 
-        setLockoutDuration( wep )
+    wep:SetClip1( savedAmmo.Clip1 )
+    wep:SetClip2( savedAmmo.Clip2 )
 
-        local nextFire = ply.WeaponLockouts[wepClass]
-        if not nextFire then return end
+    wep:SetNextPrimaryFire( savedAmmo.NextPrimaryFire )
+    wep:SetNextSecondaryFire( savedAmmo.NextSecondaryFire )
 
-        wep:SetNextPrimaryFire( nextFire )
-    end )
+    wep:SetSequence( savedAmmo.Sequence )
 end )
 
-
-hook.Add( "PlayerDroppedWeapon", "CFC_WeaponLockouts", function( ply, wep )
-    if not IsValid( wep ) then return end
-    if not IsValid( ply ) then return end
-
+hook.Add( "PlayerDroppedWeapon", "CFC_SavedAmmo_Save", function( ply, wep )
     local wepClass = wep:GetClass()
-    local lockout = wep.CFC_LockoutTime
-    ply.WeaponLockouts[wepClass] = CurTime() + lockout
+    local nextPrimary = wep:GetNextPrimaryFire()
+
+    ply.SavedAmmo[wepClass] = {
+        Clip1 = wep:Clip1(),
+        Clip2 = wep:Clip2(),
+        NextPrimaryFire = wep:GetNextPrimaryFire(),
+        NextSecondaryFire = wep:GetNextSecondaryFire(),
+        Sequence = wep:GetSequence(),
+    }
 end )
 
-
-hook.Add( "PlayerSpawn", "CFC_WeaponLockouts", function( ply )
-    ply.WeaponLockouts = {}
+hook.Add( "PlayerSpawn", "CFC_SavedAmmo_Reset", function( ply )
+    ply.SavedAmmo = {}
 end )
